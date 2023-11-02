b47c4711febf75d39bc80e5a4afecd3916afb426
arupay <arupay11374@gmail.com>
2023-11-01 23:53:11 -0400

    fixed display errors on map

--- front-end/src/Components/ShowRooms.js
+++ front-end/src/Components/ShowRooms.js
@@ -7,25 +7,27 @@ function ShowRooms(props) {
   const navigate = useNavigate();
   return (
     <div className="mt-3">
-      {rooms.map((room) => (
-        <div
-          key={room.id}
-          className="card mb-2 room-card"
-          onClick={() => navigate(`/meetingrooms/${room.id}`)}
-        >
-          <div className="card-body">
-            <h5 className="card-title">{room.name}</h5>
-            <div className="d-flex align-items-center mb-2">
-              <BsPeople size="2em" />
-              <span className="mx-2">Capacity: {room.capacity}</span>
+      {rooms.length
+        ? rooms.map((room) => (
+            <div
+              key={room.id}
+              className="card mb-2 room-card"
+              onClick={() => navigate(`/meetingrooms/${room.id}`)}
+            >
+              <div className="card-body">
+                <h5 className="card-title">{room.name}</h5>
+                <div className="d-flex align-items-center mb-2">
+                  <BsPeople size="2em" />
+                  <span className="mx-2">Capacity: {room.capacity}</span>
+                </div>
+                <div className="d-flex align-items-center">
+                  <BsBuilding size="2em" />
+                  <span className="mx-2">Floor: {room.floor}</span>
+                </div>
+              </div>
             </div>
-            <div className="d-flex align-items-center">
-              <BsBuilding size="2em" />
-              <span className="mx-2">Floor: {room.floor}</span>
-            </div>
-          </div>
-        </div>
-      ))}
+          ))
+        : "No Rooms To map"}
     </div>
   );
 }
